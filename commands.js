e4a185a8d3277cd63fdbb9c10012870eeb8db1d0
ckuudee <109048263+ckuudee@users.noreply.github.com>
2024-10-12 01:09:02 -0700

    Added new commands. Updated aboutme.

--- commands.js
+++ commands.js
@@ -1,9 +1,30 @@
+const linkedin = "https://www.linkedin.com/in/codyhoang/";
+const github = "https://github.com/ckuudee";
+const youtube = "https://www.youtube.com/@CodyandDavidProgramming-z8s";
+const resume = "https://www.resume.lol/share/hb03ict";
+const email = "mailto:codyhoang2005@gmail.com";
+
 const help = [
-    "aboutme    - About me",
+    "Press the up and down arrows for previous commands.",
+    "",
+    "aboutme    - Learn about who I am!",
+    "linkedin   - Check out my LinkedIn!",
+    "youtube    - Check out my YouTube!",
+    "github     - Check out my GitHub!",
+    "resume     - Check out my resume!",
+    "email      - Send me an email!",
+    "clear      - Clear the terminal"
 ];
 
 const aboutme = [
-    "Hi, I am Cody!",
+    `Hi, I'm Cody! I am currently a 2nd-year Computer Science student at the University of Washington. 
+     I'm currently exploring what I am the most passionate about in the CS field and recently my friend David and I
+     have been learning C++ and Unreal Engine. Along with that we co-created a YouTube channel together in hopes of
+     publishing informative videos about them as well as fostering a small community that can grow alongside us as we
+     learn too. Ultimately, my greatest passion is working on impactful projects that contribute to something larger 
+     than myself and creating solutions people never realized they needed, yet can't live without.`
 ];
 
-export {help, aboutme};
+
+
+export {linkedin, github, help, aboutme, youtube, resume, email};
